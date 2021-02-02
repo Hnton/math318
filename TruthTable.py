7555f6b8b31d4f35e90960cf5321b12663d431d2
Mikael Hinton <43019867+Hnton@users.noreply.github.com>
2021-02-02 13:37:31 -0500

    TruthTable update Lesson 3

--- TruthTable.py
+++ TruthTable.py
@@ -1,16 +1,21 @@
+# Mikael Hinton
+# getSym(x) was taken from this website: https://stackoverflow.com/questions/57862191/python-programming-truth-table
+
 def getSym(x):
     if x:
         return '| True '
     else:
         return '| False'
 
+
+
 values = [True, False]
-print(" -------------------------------")
-print("|   P  ", "|   Q  ", "| P=>Q ", "| P<=>Q |")
-print(" -------------------------------")
+print(" ---------------------------------------------------------------")
+print("|   P  ", "|   Q  ", "|  P^Q ", "| N(P^Q)|", "  NP  |","  NQ  |", "NPvNQ |", " iff  |")
+print(" ---------------------------------------------------------------")
 for p in values:
     for q in values:
-        print(getSym(p), getSym(q), getSym(not p or q), getSym((not p or q) and (not q or p)), "|")
-        print(" -------------------------------")
+        print(getSym(p), getSym(q), getSym(p and q), getSym(not (p and q)), getSym(not (p)), getSym(not (q)), getSym((not p) or (not q)), getSym(not p or q and not q or p), "|")
+        print(" ---------------------------------------------------------------")
 
 
