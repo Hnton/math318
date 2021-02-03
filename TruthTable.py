4e5bdd6a5e28f9ef0d82615dffedea1588507f3e
Mikael Hinton <43019867+Hnton@users.noreply.github.com>
2021-02-03 13:23:43 -0500

    Project 3 DONE

--- TruthTable.py
+++ TruthTable.py
@@ -1,21 +1,25 @@
 # Mikael Hinton
-# getSym(x) was taken from this website: https://stackoverflow.com/questions/57862191/python-programming-truth-table
+# Project 3
+# getValue(x) was taken from this website: https://stackoverflow.com/questions/57862191/python-programming-truth-table
 
-def getSym(x):
+
+# Function that if returns the values of either true or false 
+def getValue(x):
     if x:
         return '| True '
     else:
         return '| False'
 
 
-
+# Values of the truth table
 values = [True, False]
+# Prints the table and calls the getValue function to get either true or false
 print(" ---------------------------------------------------------------")
 print("|   P  ", "|   Q  ", "|  P^Q ", "| N(P^Q)|", "  NP  |","  NQ  |", "NPvNQ |", " iff  |")
 print(" ---------------------------------------------------------------")
 for p in values:
     for q in values:
-        print(getSym(p), getSym(q), getSym(p and q), getSym(not (p and q)), getSym(not (p)), getSym(not (q)), getSym((not p) or (not q)), getSym(not p or q and not q or p), "|")
+        print(getValue(p), getValue(q), getValue(p and q), getValue(not (p and q)), getValue(not (p)), getValue(not (q)), getValue((not p) or (not q)), getValue(not p or q and not q or p), "|")
         print(" ---------------------------------------------------------------")
 
 
