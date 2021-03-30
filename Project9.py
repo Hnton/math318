3c47505ad263e4ac1d956d1003b251267a50f54b
Mikael Hinton <43019867+Hnton@users.noreply.github.com>
2021-03-30 11:01:57 -0400

    project 9 update

--- Project9.py
+++ Project9.py
@@ -2,14 +2,33 @@
 # Project 9
 # Functions from https://stackoverflow.com/questions/37237954/calculate-the-lcm-of-a-list-of-given-numbers-in-python
 
-from math import gcd
-
 a = [9720,4158,594,612]
-lcm = a[0]
 
+def euc(a,b):
+	c=list(range (2))
+	if a>b:
+		c[0], c[1]=a, b
+	if b>a:
+		c[0], c[1]=b, a
+	while c[1]>0:
+		d=c[0]%c[1]
+		c[0]=c[1]
+		c[1]=d
+		gcd=c[0]
+		if c[1]==1:
+			gcd=1
+			break
+	return gcd
+
+
+f = euc(a[0],a[1])
+s = euc(f,a[2])
+l = euc(s,a[3])
+
+lcm = a[0]
 for i in a[1:]:
-  lcm = lcm*i//gcd(lcm, i)
+  lcm = lcm*i//euc(lcm, i)
 
 print("Least Common Multiple: ",lcm)
 
-print("Greatest Common Factor: ", gcd(*a))
+print("Greatest Common Factor: ", l)
